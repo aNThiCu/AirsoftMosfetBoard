f5de5b334bec184114d83867b6e035692b941feb
Tanasie Cezar <72690887+aNThiCu@users.noreply.github.com>
2023-09-03 14:32:07 +0300

    Add files via upload

--- CodeTriggerV.2.ino
+++ CodeTriggerV.2.ino
@@ -8,8 +8,8 @@ int BreakOutput = PIN_PB5;
 bool mode = 1;
 bool shooting = 0;
 bool brk = 0;
-//counters
-int count = 0;
+//timers
+unsigned long int startBreak=0;
 
 void setup() {
   Serial.begin(9600);
@@ -36,17 +36,17 @@ void Start_shoot() {
 
 void Cycle_finish() {
   if (shooting == true)
+  {
     if (mode == 1) {
       analogWrite(NmosOutput, 0);
       shooting = false;
       brk = 1;
-      count = 0;
     } else if (digitalRead(triggerPin) == 1) {
       analogWrite(NmosOutput, 0);
       shooting = false;
-      brk = 1;
-      count = 0;
+      brk = 1; 
     }
+  }
 }
 
 void Start_break()
@@ -61,25 +61,16 @@ void Stop_break()
   digitalWrite(BreakOutput,0);
 }
 
-unsigned long int startBreak=0;
+
+
 void loop() {
   noInterrupts();
   if (brk == 1 && digitalRead(BreakOutput)==0)
     Start_break();
   interrupts();
 
-  if(digitalRead(BreakOutput)==1 && milis()-startBreak>=100)
+  if(digitalRead(BreakOutput)==1 && millis()-startBreak>=100)
     Stop_break();
+  
   if (analogRead(modePin) > 55) mode = 0; else mode = 1;  //0=fullauto , 1=semiauto
 }
-
-/*
-ISR (XXX_vect, ISR_NAKED) {
-  asm (
-    "pop r0\n"   // remove PC value from stack
-    "pop r0\n"
-    // load new PC into regs 30 and 31 (I can't remember how to reference an external C function address)
-    "push r30\n"
-    "push r31\n"
-    "reti\n"
-  );
