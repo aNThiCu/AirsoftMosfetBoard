041c9acace5cf4652d6d461ff2c7944239d9388f
Tanasie Cezar <72690887+aNThiCu@users.noreply.github.com>
2023-09-10 02:50:19 +0300

    -added modes
    settings can be changed via the ProgrammingModule

--- CodeTriggerV_2_DetachAttachInterrupt.ino
+++ CodeTriggerV_2_DetachAttachInterrupt.ino
@@ -11,8 +11,17 @@ int BreakOutput = PIN_PB5;
 bool firingMode = 1;
 bool brk = 0;
 bool progMode=0;
+//values
+int shotCount=0;
 //saved values
-int percent;
+int   rof;
+bool  activeBreak;
+bool  preCocking;
+bool  binaryTrigger;
+bool  smartTrigger;
+int   firstPos;
+int   secondPos;
+int   burstAmount;
 //timers
 unsigned long int startBreak=0;
 
@@ -27,34 +36,47 @@ void setup() {
   //Output Value Init
   analogWrite(NmosOutput, 0);
   digitalWrite(BreakOutput, 0);
+  //Saved Values Init
+  EEPROM.get(0,rof);
+  EEPROM.get(1,activeBreak);
+  EEPROM.get(2,preCocking);
+  EEPROM.get(3,binaryTrigger);
+  EEPROM.get(4,smartTrigger);
+  EEPROM.get(5,firstPos);
+  EEPROM.get(6,secondPos);
+  EEPROM.get(7,burstAmount);
+  //Init Finish
+
   progMode=digitalRead(triggerPin);
   
   delay(5000);
   if(progMode)
   {
-  percent=EEPROM.read(0);
-  attachInterrupt(digitalPinToInterrupt(triggerPin), Start_shoot, FALLING);
+  if(binaryTrigger==true)
+      attachInterrupt(digitalPinToInterrupt(triggerPin), Start_shoot, CHANGE);
+    else
+      attachInterrupt(digitalPinToInterrupt(triggerPin), Start_shoot, FALLING);
   }
   else
   attachInterrupt(digitalPinToInterrupt(triggerPin), Settings,FALLING);
 }
 
 void Settings(){
-  percent=EEPROM.read(0);
+  rof=255;
   switch(firingMode){
     case 0:
-    if(percent<255)
+    if(rof<255)
     {
-      percent+=5;
-      EEPROM.write(0,percent);
+      rof+=5;
+      EEPROM.put(0,rof);
     }
     break;
 
     case 1:
-    if(percent>100)
+    if(rof>100)
     {
-      percent-=5;
-      EEPROM.write(0,percent);
+      rof-=5;
+      EEPROM.put(0,rof);
     break;
   }
 }
@@ -64,7 +86,10 @@ void Start_shoot() {
 
   brk = 0;
   digitalWrite(BreakOutput, 0);
+  if(smartTrigger==true)
   analogWrite(NmosOutput, 255);
+  else
+  analogWrite(NmosOutput, rof);
 
   attachInterrupt(digitalPinToInterrupt(cyclePin), Cycle_finish, RISING);
 }
@@ -72,6 +97,35 @@ void Start_shoot() {
 void Cycle_finish() {
 
   detachInterrupt(digitalPinToInterrupt(cyclePin));
+
+  switch(firingMode){
+    
+    case 1://semi-auto
+    analogWrite(NmosOutput, 0);
+    brk = 1; 
+    break;
+
+    case 2://full-auto
+    if(digitalRead(triggerPin)==1){
+      analogWrite(NmosOutput, 0);
+      brk = 1;
+    }
+    else
+    analogWrite(NmosOutput,rof);
+    break;
+    
+    case 3://burst
+    if(shotCount>=3){
+      analogWrite(NmosOutput, 0);
+      brk = 1;
+    }
+    else
+    {
+    shotCount++;
+    analogWrite(NmosOutput,rof);
+    }
+    break;
+  }
 
   if (firingMode == 1) {//Semi-auto
     analogWrite(NmosOutput, 0);
@@ -81,7 +135,21 @@ void Cycle_finish() {
     brk = 1; 
   }
 
-  attachInterrupt(digitalPinToInterrupt(triggerPin), Start_shoot, FALLING);
+  if(binaryTrigger==true)
+      attachInterrupt(digitalPinToInterrupt(triggerPin), Start_shoot, CHANGE);
+    else
+      attachInterrupt(digitalPinToInterrupt(triggerPin), Start_shoot, FALLING);
+}
+
+void Check_break()
+{
+  noInterrupts();
+  if (brk == 1 && digitalRead(BreakOutput)==0)
+    Start_break();
+  interrupts();
+
+  if(digitalRead(BreakOutput)==1 && millis()-startBreak>=100)
+    Stop_break();
 }
 
 void Start_break()
@@ -97,13 +165,6 @@ void Stop_break()
 }
 
 void loop() {
-  noInterrupts();
-  if (brk == 1 && digitalRead(BreakOutput)==0)
-    Start_break();
-  interrupts();
-
-  if(digitalRead(BreakOutput)==1 && millis()-startBreak>=100)
-    Stop_break();
-
-  if (analogRead(modePin) > 55) firingMode = 0; else firingMode = 1;  //0=fullauto , 1=semiauto
+  if (activeBreak==true) Check_break();
+  if (analogRead(modePin) > 55) firingMode = secondPos; else firingMode = firstPos;  //2=fullauto , 1=semiauto
 }
