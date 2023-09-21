a3d2368f2a02051e5c896bacc9b00b4e893a65b4
Tanasie Cezar <72690887+aNThiCu@users.noreply.github.com>
2023-09-21 20:44:58 +0300

    Add files via upload

--- ProgrammingModule.ino
+++ ProgrammingModule.ino
@@ -8,10 +8,22 @@ bool  preCocking=false;      //stops the motor in a cocked position , improves t
 bool  binaryTrigger=false;   //fires on both pressing and releasing the trigger , *3
 bool  smartTrigger=false;    //first shot's rpm is max speed the next is the ROF's value ,better trigger response , *4
 
+//inp
+int triggerPin = PIN_PC0;
+int cyclePin = PIN_PA1;
+//out
+int NmosOutput = PIN_PA5;
+
 int   firstPos=1;   //Firing mode positions , *5
 int   secondPos=2;  //1=Semi-Auto,2=Full-Auto,3=Burst , *6
 
 int   burstAmount=3; //number of bb's shot in a burst,MAX=5, *7
+int   rps=0;
+
+bool shooting=false;
+
+unsigned long int startTime;
+
 
 void setup() {
   EEPROM.put(0,rof);
@@ -22,7 +34,33 @@ void setup() {
   EEPROM.put(5,firstPos);
   EEPROM.put(6,secondPos);
   EEPROM.put(7,burstAmount);
+  Serial.println("Settings have been saved!");
+  attachInterrupt(digitalPinToInterrupt(triggerPin), Start_shoot, FALLING);
+  attachInterrupt(digitalPinToInterrupt(cyclePin), Cycle_finish, RISING);
+  Serial.begin(9600);
+}
+
+//Code for Rounds per Second
+   |
+ \ | /
+  \_/
+void Start_shoot(){
+  detachInterrupt(digitalPinToInterrupt(triggerPin));
+  shooting=true;
+  analogWrite(NmosOutput,rps);
+}
+
+void Cycle_finish(){
+  if(rps==0)
+  startTime=millis();
+  rps++;
 }
 
 void loop() {
+  if((millis()-startTime>=1000) && shooting == true){
+  EEPROM.put(8,rps);
+  Serial.println(rps);
+  analogWrite(NmosOutput,0);
+  shooting=false;
+  }
 }
